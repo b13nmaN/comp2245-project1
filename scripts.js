7265806934a4d108cfa4a018c9081c3f59bc0a77
b13nmaN <blenmanjoel@gmail.com>
2023-11-03 02:00:17 -0400

    added form validation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function () {
+  const messageDiv = document.querySelector(".message");
+  const btn = document.querySelector("form .btn");
+
+  btn.addEventListener("click", function (event) {
+    event.preventDefault(); 
+    const emailInput = document.querySelector("#email");
+    const email = emailInput.value; 
+    DisplayMessage(email); 
+  });
+
+  function DisplayMessage(emailAddress) {
+    if (isValidEmail(emailAddress)) {
+      messageDiv.textContent = `Thank you! Your email address ${emailAddress} has been added to our mailing list!`;
+      messageDiv.style.color = "white";
+    } else {
+      messageDiv.textContent = "Please enter a valid email address.";
+      messageDiv.style.color = "red";
+    }
+  }
+
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+});
